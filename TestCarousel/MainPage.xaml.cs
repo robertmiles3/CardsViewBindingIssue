1f29f65493d8f9f9311517ded14ba531bb36da3e
Robert Miles <rmiles@sjvassoc.com>
2018-06-22 08:19:35 -0400

    Initial

---
+++ TestCarousel/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestCarousel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            
+
+            var viewModel = new MainViewModel
+            {
+                Stats = new DashboardStats
+                {
+                    Count1 = 111,
+                    Count2 = 222,
+                    Count3 = 333,
+                    Count4 = 444,
+                    Count5 = 555,
+                    Count6 = 666,
+                    Count7 = 777,
+                    Count8 = 888
+                }
+            };
+            BindingContext = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestCarousel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MvvmHelpers;
+
+namespace TestCarousel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private DashboardStats stats;
+        public DashboardStats Stats
+        {
+            get => stats;
+            set
+            {
+                SetProperty(ref stats, value);
+            }
+        }
+    }
+
+    public class DashboardStats
+    {
+        public int Count1 { get; set; }
+        public int Count2 { get; set; }
+        public int Count3 { get; set; }
+        public int Count4 { get; set; }
+
+        public int Count5 { get; set; }
+        public int Count6 { get; set; }
+        public int Count7 { get; set; }
+        public int Count8 { get; set; }
+    }
+}
